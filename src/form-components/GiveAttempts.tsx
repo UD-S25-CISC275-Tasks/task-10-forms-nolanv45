afc7cd45de99cc33faa838006da64a237b57d16d
Nolan <nolanv@udel.edu>
2025-03-13 22:28:02 -0400

    check answer and give attempts work

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,44 @@
 import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [requests, setRequests] = useState<string>("");
+
+
+    function setRequestsHandler(event: React.ChangeEvent<HTMLInputElement>) {
+        setRequests(event.target.value);
+    }
+
+    function handleUse() {
+         if (attempts > 0) {
+            setAttempts(attempts - 1);
+        }
+    }
+
+    function handleGain() {
+        const numRequests = parseInt(requests, 10);
+        if (!isNaN(numRequests) && numRequests > 0) {
+            setAttempts(attempts + numRequests);
+        }
+        setRequests(""); 
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <p>Attempts Remaining: {attempts}</p>
+            <Form.Group controlId="attemptRequest">
+            <Form.Label>Request Attempts:</Form.Label>
+            <Form.Control type="text" value={requests} onChange={setRequestsHandler} />
+            </Form.Group>
+            <p>Attempts: {attempts} </p>
+            <Button onClick={handleUse} disabled={attempts === 0}>
+                Use
+            </Button>
+            <Button onClick={handleGain}>
+                Gain
+            </Button>
         </div>
     );
 }
